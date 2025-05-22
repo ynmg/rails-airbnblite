df7ed84b73055945a009c7a0ea30ae5732b2a1a9
Nga Man Yeung(Grace) <graceyeungnm@gmail.com>
2025-05-22 12:45:03 +0100

    Added image_url column to listings, updated seed file with image_url, updated image path in listing/index+show & booking/index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_21_114507) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_22_111455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_21_114507) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
