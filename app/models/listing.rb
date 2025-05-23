337e738d336fe7c7399e5f16b6b962a46b368403
Nga Man Yeung(Grace) <graceyeungnm@gmail.com>
2025-05-23 13:57:43 +0100

    Added max length for address, fixed path issues with button on homepage, changed seed files, listing names and description

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ApplicationRecord
-  
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
@@ -17,7 +17,7 @@ class Listing < ApplicationRecord
 
   validates :name, presence: true
   validates :location, presence: true
-  validates :description, presence: true
+  validates :description, presence: true, length: { maximum: 200 }
   validates :number_guests, presence: true, numericality: { only_integer: true }
 # greater_than 0
   validates :price, presence: true
